fcadc79e1e43d700ffb5f2a41c6384369af224d9
Brett Drainer <bdrainer@fairwaytech.com>
2017-03-02 13:21:53 -0800

    IAT-143:  Added Swagger for documenting the REST APIs.

--- src/main/java/ap/ims/config/SecurityConfig.java
+++ src/main/java/ap/ims/config/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class IMSSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(final HttpSecurity http) throws Exception {
