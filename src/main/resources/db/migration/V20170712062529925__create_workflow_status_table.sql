3d7d529d6cf743095bed585d6601bfd571208266
Brett Drainer <brett@gearlaunch.com>
2017-07-14 13:23:38 -0700

    IAT-539:  Updated the insert of the workflow_statuses so it is a bit cleaner

--- src/main/resources/db/migration/V20170712062529925__create_workflow_status_table.sql
+++ src/main/resources/db/migration/V20170712062529925__create_workflow_status_table.sql
@@ -6,27 +6,29 @@ CREATE TABLE IF NOT EXISTS `iat`.`workflow_status` (
   PRIMARY KEY (`code`)
 );
 
-
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('Draft', '1. Draft', 1);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('InitialReview', '2. Initial Review', 2);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('MultimediaUpload', '2a. Multimedia Upload', 3);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('AuditReview', '3. Audit Review', 4);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('QualityReview', '4. Quality Review', 5);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('QualityCorrections', '4a. Quality Corrections', 6);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('QualityContentReview', '4b. Quality Content Review', 7);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('QualityStudentSupportReview', '4c. Quality Student Support Review', 8);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('ContentEditReview', '5. Content Edit Review', 9);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('ContentReview', '6. Content Review', 10);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('TextToSpeechUpload', '7. Text-to-speech Upload', 11);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('AccessibilityUpload', '8. Accessibility Upload', 12);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('AccessibilityReview', '9. Accessibility Review', 13);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('ContentFinalReview', '10. Content Final Review', 14);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('FieldTest', '11. Field Test', 15);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('PsychometricReview', '12. Psychometric Review', 16);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('DataReview', '12a. Data Review', 17);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('PostFieldTestCorrections', '12b. Post Field Test Corrections', 18);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('Operational', '13. Operational', 19);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('Released', '14. Released', 20);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('Archived', '15. Archived', 21);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('Rejected', 'Rejected', 900);
-INSERT IGNORE INTO `iat`.`workflow_status` (`code`, `label`, `workflow_order`) VALUES('DoNotUse', 'Do Not Use', 901);
+INSERT IGNORE INTO
+  `iat`.`workflow_status` (`code`, `label`, `workflow_order`)
+VALUES
+  ('Draft', '1. Draft', 1),
+  ('InitialReview', '2. Initial Review', 2),
+  ('MultimediaUpload', '2a. Multimedia Upload', 3),
+  ('AuditReview', '3. Audit Review', 4),
+  ('QualityReview', '4. Quality Review', 5),
+  ('QualityCorrections', '4a. Quality Corrections', 6),
+  ('QualityContentReview', '4b. Quality Content Review', 7),
+  ('QualityStudentSupportReview', '4c. Quality Student Support Review', 8),
+  ('ContentEditReview', '5. Content Edit Review', 9),
+  ('ContentReview', '6. Content Review', 10),
+  ('TextToSpeechUpload', '7. Text-to-speech Upload', 11),
+  ('AccessibilityUpload', '8. Accessibility Upload', 12),
+  ('AccessibilityReview', '9. Accessibility Review', 13),
+  ('ContentFinalReview', '10. Content Final Review', 14),
+  ('FieldTest', '11. Field Test', 15),
+  ('PsychometricReview', '12. Psychometric Review', 16),
+  ('DataReview', '12a. Data Review', 17),
+  ('PostFieldTestCorrections', '12b. Post Field Test Corrections', 18),
+  ('Operational', '13. Operational', 19),
+  ('Released', '14. Released', 20),
+  ('Archived', '15. Archived', 21),
+  ('Rejected', 'Rejected', 900),
+  ('DoNotUse', 'Do Not Use', 901);
